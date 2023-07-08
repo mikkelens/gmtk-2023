e21f0bb458525564b515fd39655a9bd189b2de5b
Mikkel <Mikkel123sej@gmail.com>
2023-07-08 18:47:04 +0200

    made the wire rotate and scale

---
+++ Assets/StretchWireScript.cs
@@ -0,0 +1,19 @@
+﻿using Tools.Utils;
+using UnityEngine;
+
+public class StretchWireScript : MonoBehaviour
+{
+	// call each frame
+	public void StretchBetween(Vector2 source, Vector2 destination)
+	{
+		Transform myTransform = transform;
+
+		Vector2 destinationVector = destination - source;
+
+		Quaternion rotation = Quaternion.FromToRotation(Vector3.right, destinationVector.normalized);
+		myTransform.localRotation = rotation;
+
+		RectTransform myRectTransform = myTransform.GetComponent<RectTransform>();
+		myRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, destinationVector.magnitude);
+	}
+}
--- Assets/WireButtonLeft.cs
+++ Assets/WireButtonLeft.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class WireButtonLeft : MonoBehaviour
     public bool pressed;
     [SerializeField] GameObject cursor;
     [SerializeField] GameObject wire;
+    [SerializeField] private StretchWireScript wireScript;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,20 @@ public class WireButtonLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
+	    Vector2 target = Input.mousePosition;
+	    Vector2 source = transform.position;
+	    wireScript.StretchBetween(source, target);
+    }
+
+    private void OnDrawGizmos()
+    {
+	    Vector2 source = transform.position;
+	    Vector2 target = Input.mousePosition;
+	    Gizmos.color = Color.blue;
+	    Gizmos.DrawWireSphere(source, 10f);
+	    Gizmos.color = Color.white;
+	    Gizmos.DrawLine(source, target);
+	    Gizmos.color = Color.red;
+	    Gizmos.DrawWireSphere(target, 10f);
     }
 }

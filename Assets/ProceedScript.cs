a13f746f8ee18aa3f0e153c556241919acb79d3a
leonardzz7 <leonard@leooli.dk>
2023-07-09 04:49:35 +0200

    Queue Half Done

--- Assets/ProceedScript.cs
+++ Assets/ProceedScript.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core;
 
 public class ProceedScript : MonoBehaviour
 {
+    public float susToAdd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.localPosition = new Vector2(Random.Range(-850, 851), Random.Range(-450, 451));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        susToAdd = susToAdd + 0.02f;
     }
 
     public void Spawn()
     {
-        gameObject.transform.position = new Vector2(Random.Range(-850,851), Random.Range(-450, 451));
+        
+    }
+
+    public void Pressed()
+    {
+        PersistentGameManager.Instance.SusMeter += ((int)susToAdd);
+        Destroy(gameObject);
     }
 }

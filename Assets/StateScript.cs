db1b96bee73aa6eeb80c98c42856d13890302e05
leonardzz7 <leonard@leooli.dk>
2023-07-09 15:21:46 +0200

    Queue Game Finished

--- Assets/StateScript.cs
+++ Assets/StateScript.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core;
 
 public class StateScript : MonoBehaviour
 {
-    public bool proceedState;
+    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject canvas;
+    public bool proceedState = false;
+    bool spawned = false;
+    public int waits;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +19,33 @@ public class StateScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (proceedState)
+        if (waits < 4)
         {
-
+            if (proceedState)
+            {
+                spawned = false;
+            }
+            else
+            {
+                if (!spawned) { spawned = true; StartCoroutine(Waiter()); }
+                if (Input.anyKeyDown || Input.GetAxis("Mouse X") != 0) { PersistentGameManager.Instance.SusMeter += 2; }
+            }
         }
         else
         {
 
+
         }
     }
+
+    private IEnumerator Waiter()
+    {
+        
+        yield return new WaitForSeconds(Random.Range(25,36));
+        proceedState = true;
+        var button = Instantiate(prefab);
+        button.transform.parent = canvas.gameObject.transform;
+    }
+
+   
 }
